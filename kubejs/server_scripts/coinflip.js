d61d68f68160ade940202ec93fb29d3847e4a299
Maxwell Chow <maxchowla@gmail.com>
2026-07-16 04:15:26 -0700

    fix coinflip event

--- kubejs/server_scripts/coinflip.js
+++ kubejs/server_scripts/coinflip.js
@@ -1,5 +1,6 @@
 console.info("[KITCHEN] Coinflip script loaded");
 
-ItemEvents.rightClicked("gamble_coinflip", (event) => {
-	server.tell(event.player.username + " flipped a coin")
-})
+ItemEvents.rightClicked("kubejs:gamble_coinflip", (event) => {
+	event.server.tell(event.player.username + " flipped a coin");
+	console.info("[KITCHEN] coin clicked");
+});
