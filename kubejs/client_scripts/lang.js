1d48747d39ed723597a7109d5ad3e1fb6df40311
Maxwell Chow <maxchowla@gmail.com>
2026-07-21 19:51:39 -0700

    pack specific localization

---
+++ kubejs/client_scripts/lang.js
@@ -0,0 +1,14 @@
+ClientEvents.lang('en_us', event => {
+	event.renameItem("numismatics:spur", "Copper Penny");
+	event.renameItem("numismatics:bevel", "Zinc Eighth");
+	event.renameItem("numismatics:sprocket", "Iron Quarter");
+	event.renameItem("numismatics:cog", "Brass Dollar");
+	event.renameItem("numismatics:crown", "Gold Eight");
+	event.renameItem("numismatics:sun", "Netherite Coin");
+	event.renameItem("create:sweet_roll", "Cumbread");
+	event.renameItem("kubejs:gamble_coinflip", "Coinflip Token");
+	event.renameItem("kubejs:gamble_penguin", "Penguin Token");
+	event.renameItem("kubejs:gamble_dragon", "Dragon Token");
+	event.renameItem("kubejs:gamble_roulette", "Roulette Token");
+	event.renameItem("numismatics:creative_vendor", "Server Vendor");
+})
--- kubejs/startup_scripts/items.js
+++ kubejs/startup_scripts/items.js
@@ -3,5 +3,8 @@ console.info("[KITCHEN] Item registry loaded");
 StartupEvents.registry('item', event => {
 
 	event.create("gamble_coinflip").unstackable();
+	event.create("gamble_penguin").unstackable();
+	event.create("gamble_dragon").unstackable();
+	event.create("gamble_roulette").unstackable();
 
 });
