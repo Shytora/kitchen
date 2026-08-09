8260f7d288d7b4ccddb89340c66533fd34b4840f
Shytora <acidraeyn1@gmail.com>
2026-08-09 00:11:06 -0700

    Update refunds.js
    refunds added

--- kubejs/server_scripts/refunds.js
+++ kubejs/server_scripts/refunds.js
@@ -1,9 +1,99 @@
 ServerEvents.recipes(event => {
 
 	event.shapeless(
+		Item.of("numismatics:cog", 12),
+		["prefab:item_aqua_base"]
+	);
+	event.shapeless(
+		Item.of("numismatics:cog", 24),
+		["prefab:item_aqua_base_improved"]
+	);
+	event.shapeless(
+		Item.of("numismatics:cog", 8),
+		["prefab:item_warehouse"]
+	);
+	event.shapeless(
+		Item.of("numismatics:cog", 32),
+		["prefab:item_warehouse_improved"]
+	);
+	event.shapeless(
+		Item.of("numismatics:cog", 4),
+		["prefab:item_farm"]
+	);
+	event.shapeless(
+		Item.of("numismatics:cog", 12),
+		["prefab:item_farm_improved"]
+	);
+	event.shapeless(
+		Item.of("numismatics:sun", 1),
+		["prefab:item_farm_advanced"]
+	);
+	event.shapeless(
+		Item.of("numismatics:cog", 4),
+		["prefab:item_modern_buildings"]
+	);
+	event.shapeless(
+		Item.of("numismatics:cog", 8),
+		["prefab:item_modern_buildings_improved"]
+	);
+	event.shapeless(
+		Item.of("numismatics:cog", 36),
+		["prefab:item_modern_buildings_advanced"]
+	);
+	event.shapeless(
 		Item.of("numismatics:cog", 4),
 		["prefab:item_house"]
 	);
-
-
+	event.shapeless(
+		Item.of("numismatics:cog", 12),
+		["prefab:item_house_improved"]
+	);
+	event.shapeless(
+		Item.of("numismatics:cog", 36),
+		["prefab:item_house_advanced"]
+	);
+	event.shapeless(
+		Item.of("numismatics:cog", 4),
+		["prefab:item_watch_tower"]
+	);
+	event.shapeless(
+		Item.of("numismatics:cog", 6),
+		["prefab:item_saloon"]
+	);
+	event.shapeless(
+		Item.of("numismatics:cog", 8),
+		["prefab:item_wind_mill"]
+	);
+	event.shapeless(
+		Item.of("numismatics:cog", 8),
+		["prefab:item_machinery_tower"]
+	);
+	event.shapeless(
+		Item.of("numismatics:cog", 8),
+		["prefab:item_town_hall"]
+	);
+	event.shapeless(
+		Item.of("numismatics:cog", 8),
+		["prefab:item_nether_gate"]
+	);
+	event.shapeless(
+		Item.of("numismatics:cog", 12),
+		["prefab:item_jail"]
+	);
+	event.shapeless(
+		Item.of("numismatics:cog", 16),
+		["prefab:item_defense_bunker"]
+	);
+	event.shapeless(
+		Item.of("numismatics:cog", 24),
+		["prefab:item_welcome_center"]
+	);
+	event.shapeless(
+		Item.of("numismatics:cog", 24),
+		["prefab:item_ski_lodge"]
+	);
+	event.shapeless(
+		Item.of("numismatics:sun", 1),
+		["prefab:item_ender_gateway"]
+	);
 });
