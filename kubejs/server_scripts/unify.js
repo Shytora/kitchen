0db968ad88f14591175da7cad987823ddc26c0d1
Shytora <acidraeyn1@gmail.com>
2026-08-21 16:18:25 -0700

    Update unify.js
    added enderman head swap

--- kubejs/server_scripts/unify.js
+++ kubejs/server_scripts/unify.js
@@ -4,3 +4,7 @@ event.add('c:nuggets/silver', 'mythicmetals:silver_nugget')
 // unifies steel nuggets
 event.add('c:nuggets/steel', 'mythicmetals:steel_nugget')
 });
+
+ServerEvents.recipes(event => {
+	event.shapeless("enderio:enderman_head", "butchery:enderman_head");
+});
