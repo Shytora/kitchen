5052057df9386496a9ca91d624448d808d2fc08c
Shytora <acidraeyn1@gmail.com>
2026-08-21 18:40:48 -0700

    Update silvlead.js
    added charcoal

--- kubejs/server_scripts/silvlead.js
+++ kubejs/server_scripts/silvlead.js
@@ -9,4 +9,15 @@ event.recipes.createCrushing([
   CreateItem.of('create:crushed_raw_lead', 0.2),
   CreateItem.of('createnuclear:lead_nugget', 0.2)
 ], 'minecraft:basalt')
+// charcoal recipes
+event.recipes.createCompacting(
+CreateItem.of('minecraft:charcoal', 0.5),
+'minecraft:bamboo_block'
+).heated();
+
+event.recipes.createCompacting(
+'minecraft:charcoal',
+'minecraft:dried_kelp_block'
+).heated();
+
 });
